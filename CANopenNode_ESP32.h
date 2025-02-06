7b33f1321ee11bfbfd63df4373688c95712f468b
Ihor Ziuba <igor.ziuba@gmail.com>
2025-02-06 10:15:26 +0100

    added readSDO and writeSDO impl

--- CANopenNode_ESP32.h
+++ CANopenNode_ESP32.h
@@ -6,7 +6,13 @@
 #include "CANopen.h"
 
 bool CO_ESP32_init();
-CO_t* CO_obtain(void);
+CO_t *CO_obtain(void);
+
+CO_SDO_abortCode_t
+read_SDO(CO_SDOclient_t *SDO_C, uint8_t nodeId, uint16_t index, uint8_t subIndex, uint8_t *buf, size_t bufSize,
+         size_t *readSize);
+CO_SDO_abortCode_t
+write_SDO(CO_SDOclient_t *SDO_C, uint8_t nodeId, uint16_t index, uint8_t subIndex, uint8_t *data, size_t dataSize);
 
 #endif /* CONFIG_USE_CANOPENNODE */
 #endif /* CANOPENNODE_ESP32_H */
